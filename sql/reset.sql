7709d0dd7899a9e4bb429e04478c076d8eb49cbe
Yashasvi-Sriram <yash.3997@gmail.com>
2017-10-16 16:39:22 +0530

    creates tables, moves not null unique constant (in time) fields to pk, rearranges table declarations w.r.t user access
    creates: tables
    	1. cinema_building_owner
    	2. cinema_building_owner_password
    	3. cinema_building
    	4. theatre
    
    moves: fields non null unique constant fields to pk
    	1. muff_id, movie_id in review
    	2. movie_id, actor_id, character_id in movie_actor_character_r

--- sql/reset.sql
+++ sql/reset.sql
@@ -8,6 +8,8 @@ DROP TABLE IF EXISTS movie;
 DROP TABLE IF EXISTS follows;
 DROP TABLE IF EXISTS movie_owner_password;
 DROP TABLE IF EXISTS movie_owner;
+DROP TABLE IF EXISTS theatre;
+DROP TABLE IF EXISTS cinema_building;
 DROP TABLE IF EXISTS cinema_building_owner_password;
 DROP TABLE IF EXISTS cinema_building_owner;
 DROP TABLE IF EXISTS muff_password;
@@ -19,32 +21,38 @@ DROP TABLE IF EXISTS muff;
   and give responsibility of the rest to the high level language above db
 */
 
-/*            */
-/* Muff stuff */
-/*            */
--- handle should not be allowed to update
--- needs complex update chains
--- takes too long db operation
--- of course a row should be allowed to delete
--- in which case all the dependents are cascaded
--- BUT,
--- If we use another field id and use that as primary key
--- then the handle field does not have any dependents in terms of fks
--- then handle being unique is enough
--- now it can be easily changed without complex update chains or much db time
+/*
+  Any not null unique field can be moved into primary key
+  But better to move it only if the fields do not tend to update in tuple's lifecycle
+  as updating fks of dependents is really messy
+*/
+
 /*
   In general if primary key is unique and constant for a tuple through out its lifetime
   (from creation to deletion) then operations are simplified. So if there is a need to update any
   field which could be a primary key, one could create a (synthetic if you may) primary key field
   which can be unique and constant through out the life time of the tuple
 */
+
+/*            */
+/* Muff stuff */
+/*            */
+-- if handle is pk handle should not be allowed to update as it needs complex update chains
+-- & takes too long db operation
+-- of course a muff should be allowed to delete
+-- in which case all the dependents are cascaded
+-- BUT,
+-- If we use another field id and use that as primary key
+-- then the handle field does not have any dependents in terms of fks
+-- then handle being unique is enough
+-- now it can be easily changed without complex update chains or much db time
 -- handle update can be provided
 CREATE TABLE muff (
   id     SERIAL,
   handle VARCHAR(50) NOT NULL,
   name   VARCHAR(50) NOT NULL,
-  UNIQUE (handle),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  UNIQUE (handle)
 );
 
 -- password update can be provided
@@ -67,16 +75,16 @@ CREATE TABLE follows (
   ON DELETE CASCADE
 );
 
-/*             */
-/* Movie stuff */
-/*             */
+/*                   */
+/* Movie Owner stuff */
+/*                   */
 -- handle update can be provided
 CREATE TABLE movie_owner (
   id     SERIAL,
   handle VARCHAR(50) NOT NULL,
   name   VARCHAR(50) NOT NULL,
-  UNIQUE (handle),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  UNIQUE (handle)
 );
 
 -- password update can be provided
@@ -124,19 +132,69 @@ CREATE TABLE character (
 -- prabhas in bahubali as shivudu (mahendra bahubali) and amarendra bahubali
 CREATE TABLE movie_actor_character_r (
   id           SERIAL,
-  movie_id     INT NOT NULL,
-  actor_id     INT NOT NULL,
-  character_id INT NOT NULL,
-  PRIMARY KEY (id),
+  movie_id     INT,
+  actor_id     INT,
+  character_id INT,
+  PRIMARY KEY (id, movie_id, actor_id, character_id),
   FOREIGN KEY (movie_id) REFERENCES movie (id)
   ON DELETE CASCADE,
   FOREIGN KEY (actor_id) REFERENCES actor (id)
   ON DELETE CASCADE,
   FOREIGN KEY (character_id) REFERENCES character (id)
-  ON DELETE CASCADE,
-  UNIQUE (movie_id, actor_id, character_id)
+  ON DELETE CASCADE
 );
 
+/*                               */
+/* Cinema (Building) Owner Stuff */
+/*                               */
+-- handle update can be provided
+CREATE TABLE cinema_building_owner (
+  id     SERIAL,
+  handle VARCHAR(50) NOT NULL,
+  name   VARCHAR(50) NOT NULL,
+  UNIQUE (handle),
+  PRIMARY KEY (id)
+);
+
+-- password update can be provided
+CREATE TABLE cinema_building_owner_password (
+  id       INT,
+  password VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id) REFERENCES cinema_building_owner (id)
+  ON DELETE CASCADE
+);
+
+CREATE TABLE cinema_building (
+  id          SERIAL,
+  owner_id    INT,
+  name        VARCHAR(50) NOT NULL,
+  --   address fields, format closest to google maps api
+  street_name VARCHAR(50) NOT NULL,
+  city        VARCHAR(50) NOT NULL,
+  state       VARCHAR(50) NOT NULL,
+  zip         VARCHAR(50) NOT NULL,
+  country     VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id, owner_id),
+  --   though even if these fields are unique they may point to same cinema building
+  --   this is the least we can do in db space
+  UNIQUE (name, street_name, city, state, zip, country),
+  FOREIGN KEY (owner_id) REFERENCES cinema_building_owner (id)
+  ON DELETE CASCADE
+);
+
+CREATE TABLE theatre (
+  cinema_building_id INT,
+  owner_id           INT,
+  screen_no          INT,
+  capacity           INT NOT NULL,
+  PRIMARY KEY (cinema_building_id, owner_id, screen_no),
+  FOREIGN KEY (cinema_building_id, owner_id) REFERENCES cinema_building (id, owner_id)
+  ON DELETE CASCADE
+);
+
+
+/* Muff stuff again */
 CREATE TABLE muff_likes_actor (
   muff_id  INT,
   actor_id INT,
@@ -159,11 +217,11 @@ CREATE TABLE muff_likes_character (
 
 CREATE TABLE review (
   id        SERIAL,
-  muff_id   INT           NOT NULL,
-  movie_id  INT           NOT NULL,
+  muff_id   INT,
+  movie_id  INT,
   rating    NUMERIC(4, 2) NOT NULL CHECK (rating >= 0.00 AND rating <= 10.00), -- Ex: 07.42 / 10.00
   timestamp TIMESTAMP     NOT NULL,
-  PRIMARY KEY (id),
+  PRIMARY KEY (id, muff_id, movie_id),
   FOREIGN KEY (muff_id) REFERENCES muff (id)
   ON DELETE CASCADE,
   FOREIGN KEY (movie_id) REFERENCES movie (id)
@@ -173,25 +231,4 @@ CREATE TABLE review (
     This is to give freedom to user to give multiple reviews based on his mood
     The avg of those reviews give a better measure of likeness of movie
   */
-);
-
-/*               */
-/* Booking Stuff */
-/*               */
--- handle update can be provided
-CREATE TABLE cinema_building_owner (
-  id     SERIAL,
-  handle VARCHAR(50) NOT NULL,
-  name   VARCHAR(50) NOT NULL,
-  UNIQUE (handle),
-  PRIMARY KEY (id)
-);
-
--- password update can be provided
-CREATE TABLE cinema_building_owner_password (
-  id       INT,
-  password VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES cinema_building_owner (id)
-  ON DELETE CASCADE
 );
