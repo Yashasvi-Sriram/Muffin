f05655bd05acb755f86aa7182c9e8db65d6be95f
Yashasvi-Sriram <yash.3997@gmail.com>
2017-10-28 20:25:44 +0530

    creates MovieDAOImpl

--- src/org/muffin/muffin/Scratch.java
+++ src/org/muffin/muffin/Scratch.java
@@ -8,14 +8,17 @@ import org.muffin.muffin.responsetypes.ArrayResponse;
 import org.muffin.muffin.responsetypes.ObjectResponse;
 import org.muffin.muffin.responsetypes.StringResponse;
 
+import java.time.LocalDateTime;
+
 public class Scratch {
     public static void main(String[] args) {
-        Gson gson = new GsonBuilder().create();
-        String a = gson.toJson(ObjectResponse.get(new JsonObject()));
-        String b = gson.toJson(ArrayResponse.get(new JsonArray()));
-        String c = gson.toJson(StringResponse.error("String response"));
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+//        Gson gson = new GsonBuilder().create();
+//        String a = gson.toJson(ObjectResponse.get(new JsonObject()));
+//        String b = gson.toJson(ArrayResponse.get(new JsonArray()));
+//        String c = gson.toJson(StringResponse.error("String response"));
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        LocalDateTime localDateTime = LocalDateTime.ofEpochSecond()
     }
 }
---
+++ src/org/muffin/muffin/daoimplementations/MovieDAOImpl.java
@@ -0,0 +1,13 @@
+package org.muffin.muffin.daoimplementations;
+
+import org.muffin.muffin.beans.Movie;
+import org.muffin.muffin.daos.MovieDAO;
+
+import java.util.List;
+
+public class MovieDAOImpl implements MovieDAO {
+    @Override
+    public List<Movie> getByOwner(String ownerHandle) {
+        return null;
+    }
+}
