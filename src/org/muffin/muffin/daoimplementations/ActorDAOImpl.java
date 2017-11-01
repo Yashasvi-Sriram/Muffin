2018c3bbf0814c1a21ec629c7053c90ddfde8994
Anirudh <anirudhreddyk98@gmail.com>
2017-11-01 21:39:12 +0530

    removing the optional feature of search

--- src/org/muffin/muffin/daoimplementations/ActorDAOImpl.java
+++ src/org/muffin/muffin/daoimplementations/ActorDAOImpl.java
@@ -12,23 +12,20 @@ import java.util.Optional;
 
 public class ActorDAOImpl implements ActorDAO {
     @Override
-    public Optional<List<Actor>> search(String searchkey) {
+    public List<Actor> search(String searchkey) {
         List<Actor> actorList = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(DBConfig.URL, DBConfig.USERNAME, DBConfig.PASSWORD);
              PreparedStatement preparedStmt = conn.prepareStatement("SELECT * FROM actor WHERE name ilike ?")) {
             preparedStmt.setString(1, "%" + searchkey + "%");
             ResultSet resultSet = preparedStmt.executeQuery();
-
             while (resultSet.next()) {
                 Actor actor = new Actor(resultSet.getInt(1), resultSet.getString(2));
                 actorList.add(actor);
             }
-            return Optional.ofNullable(actorList);
-
         } catch (SQLException e) {
             e.printStackTrace();
-            return Optional.empty();
-        }
 
+        }
+        return actorList;
     }
 }
