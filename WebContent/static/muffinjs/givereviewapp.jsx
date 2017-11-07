093b35d3bd7fb9232603393a79b79f4dd2811c6e
akshithreddy <dsarrr10@gmail.com>
2017-11-07 17:57:58 +0530

    modifies movie autocomplete suggestions in review

--- WebContent/static/muffinjs/givereviewapp.jsx
+++ WebContent/static/muffinjs/givereviewapp.jsx
@@ -26,13 +26,124 @@ let isReviewValid = function (name, rating, review) {
     return true;
 };
 
+let SearchResultWrapper = React.createClass({
+            render: function () {
+                return (
+                    <tr>
+                        <td onClick={e => this.props.onItemClick(this.props.name)}>{this.props.name}</td>
+                    </tr>
+                );
+            }
+            });
+
 window.GiveReviewApp = React.createClass({
+
+	getInitialState: function () {
+        return {
+            results: [],
+            offset: 0,
+        }
+    },
     getDefaultProps: function () {
         return {
             contextPath: '',
             url: '/review/create',
+           	limit: 3,
+            
         }
     },
+     _resetOffset: function () {
+        this.state.offset = 0;
+    },
+    _incrementOffset: function (fetchedDataLength) {
+        // last batch
+        if (fetchedDataLength < this.props.limit) {
+            this.state.offset -= this.props.limit;
+            this.state.offset += fetchedDataLength;
+        }
+        // update limit
+        this.state.offset += this.props.limit;
+    },
+    _decrementOffset: function () {
+        let floorExcess = this.state.offset % this.props.limit;
+        this.state.offset -= floorExcess;
+        if (floorExcess === 0) {
+            let prevOffset = this.state.offset - 2 * this.props.limit;
+            this.state.offset = prevOffset < 0 ? 0 : prevOffset;
+        } else {
+            let prevOffset = this.state.offset - this.props.limit;
+            this.state.offset = prevOffset < 0 ? 0 : prevOffset;
+        }
+    },
+    fetchNextBatch: function (pattern) {
+        let url = this.props.contextPath + '/movie/search';
+        let self = this;
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: {pattern: pattern, offset: self.state.offset, limit: self.props.limit},
+            success: function (r) {
+                let json = JSON.parse(r);
+                if (json.status === -1) {
+                    Materialize.toast(json.error, 2000);
+                }
+                else {
+                    let data = json.data;
+                    self._incrementOffset(data.length);
+                    // no results
+                    if (data.length === 0) {
+                        Materialize.toast('End of search!', 2000);
+                        return;
+                    }
+                    // add results
+                    self.setState(ps => {
+                        return {results: data};
+                    });
+                    $(self.refs.results).show();
+                }
+            },
+            error: function (data) {
+                Materialize.toast('Server Error', 2000);
+            }
+        });
+    },
+    fetchPreviousBatch: function (pattern) {
+        this._decrementOffset();
+        if (this.state.offset === 0) {
+            Materialize.toast('Start of search!', 2000);
+        }
+        this.fetchNextBatch(pattern);
+    },
+    onRegexInputKeyDown: function (e) {
+        switch (e.keyCode || e.which) {
+            // Enter Key
+            case 13:
+                let self = this;
+                this._resetOffset();
+                self.setState({results: []});
+                this.fetchNextBatch(e.target.value);
+                break;
+            // Escape key
+            case 27:
+                $(this.refs.results).hide();
+                break;
+            // Page Up key
+            case 33:
+                this.fetchPreviousBatch(this.refs.name.value);
+                break;
+            // Page Down key
+            case 34:
+                this.fetchNextBatch(this.refs.name.value);
+                break;
+            default:
+                break;
+        }
+    },
+    selectMovie: function (text) {
+                    this.refs.name.value = text;
+					$(this.refs.results).hide();
+                    
+                },
     addMovieReview: function () {
         // validation
         if (!isReviewValid(this.refs.name.value, this.refs.rating.value, this.refs.textReview.value)) {
@@ -64,6 +175,16 @@ window.GiveReviewApp = React.createClass({
         });
     },
     render: function () {
+    
+    	let results = this.state.results.map(movie => {
+            return <SearchResultWrapper
+                key={movie.id}
+                id={movie.id}
+                name={movie.name}
+                onItemClick={this.selectMovie}
+               
+            />;
+        });
         return (
             <div ref="form" className="row">
                 <div className="input-field col s12">
@@ -79,7 +200,7 @@ window.GiveReviewApp = React.createClass({
                         </textarea>
                 </div>
                 <div className="input-field col s6">
-                    <input type="text" ref="name" placeholder="Name of the movie" defaultValue=""/>
+                    <input type="text" ref="name" placeholder="Name of the movie" onKeyDown={this.onRegexInputKeyDown} defaultValue=""/>
                 </div>
                 <div className="input-field col s6">
                     <button onClick={this.addMovieReview}
@@ -87,7 +208,22 @@ window.GiveReviewApp = React.createClass({
                         <i className="material-icons">send</i>
                     </button>
                 </div>
+                <div ref="results">
+                            <button className="btn btn-flat"
+                                    onClick={e => this.fetchPreviousBatch(this.refs.name.value)}><i
+                                className="material-icons">keyboard_arrow_left</i></button>
+                            <button className="btn btn-flat"
+                                    onClick={e => this.fetchNextBatch(this.refs.name.value)}><i
+                                className="material-icons">keyboard_arrow_right</i></button>
+                            <span>Or use PageUp | PageDown to navigate</span>
+                            <table className="white">
+                                <tbody>{results}</tbody>
+                            </table>
+                        </div>
             </div>
         );
-    }
+    },
+     componentDidMount: function () {
+                $(this.refs.results).hide();
+            },
 });
