7dd974146d02919e51d7781800e5ceac95805bd2
Yashasvi-Sriram <yash.3997@gmail.com>
2017-11-24 21:13:06 +0530

    adds check name <> '' for movie table

--- sql/reset.sql
+++ sql/reset.sql
@@ -114,9 +114,9 @@ CREATE TABLE movie_owner_password (
 -- name update can be allowed
 CREATE TABLE movie (
   id       SERIAL,
-  name     VARCHAR(200) NOT NULL,
-  owner_id INT         NOT NULL,
-  duration INT         NOT NULL,
+  name     VARCHAR(200) NOT NULL CHECK (name <> ''),
+  owner_id INT          NOT NULL,
+  duration INT          NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (name),
   UNIQUE (id, owner_id),
